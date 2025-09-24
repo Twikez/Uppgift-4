d38aa42123c991e43176b671ad689fe3e2baa7c6
Tomas Panyokaew <tomaspanyokaew@hotmail.com>
2025-09-24 22:35:30 +0200

    hämta DOM-element

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-alert("hello")
+const taskInput = document.getElementById("taskInput");
+const addBtn = document.getElementById("addButton");
+const taskList = document.getElementById("taskList");
+const doneCount = document.getElementById("doneCount");
+
+
