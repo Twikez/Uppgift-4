0ebbf5401097f08a3d801aeab510f29bcbfee78e
Tomas Panyokaew <tomaspanyokaew@hotmail.com>
2025-09-28 21:55:10 +0200

    implementera tilläggsuppgift med validering

--- script.js
+++ script.js
@@ -3,5 +3,26 @@ const addButton = document.getElementById("addButton");
 const taskList = document.getElementById("taskList");
 const doneCount = document.getElementById("doneCount");
 
+let tasks = []; // Array för att lagra uppgifter
+
+// Lägg till uppgift
+addButton.addEventListener("click", () => {
+  const text = taskInput.value.trim();
+
+  if (text === "") {
+    alert("Du måste skriva något!");
+    return;
+  }
+
+  const task = {
+    text: text,
+    done: false
+  };
+
+  tasks.push(task);
+  renderTasks();
+  taskInput.value = "";
+});
+
 
 
